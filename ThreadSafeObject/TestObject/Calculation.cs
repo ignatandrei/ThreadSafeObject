86fe25344e1e187d2608a6e8290d3da32b6ae93e
Richard Deeming <richard@nevaleebusinesssolutions.co.uk>
2017-07-19 21:22:28 +0100

    Adding support for properties and indexers; Making fields of the ThreadSafe class readonly;

--- ThreadSafeObject/TestObject/Calculation.cs
+++ ThreadSafeObject/TestObject/Calculation.cs
@@ -9,21 +9,37 @@ namespace TestObject
     public class Calculation
     {
         public int i;
+
+        public int Value
+        {
+            get { return i; }
+            set { i = value; }
+        }
+
+        public int this[string key]
+        {
+            get { return i; }
+            set { i = value; }
+        }
+
         public int Add()
         {
             i++;
             return i;
         }
+
         public int Operation(int j)
         {
             i += j;
             return i;
         }
+
         public int Decrease()
         {
             i--;
             return i;
         }
+
         public int TwoOperations()
         {
             Add();
--- ThreadSafeObject/ThreadSafeObject/ThreadSafe.cs
+++ ThreadSafeObject/ThreadSafeObject/ThreadSafe.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ThreadSafeObject
 {
@@ -16,74 +12,153 @@ namespace ThreadSafeObject
     /// Means .NET Framework>=1.0
     /// Test and details at https://github.com/ignatandrei/ThreadSafeObject
     /// </summary>
-    public class ThreadSafe: DynamicObject,IDynamicMetaObjectProvider
+    public class ThreadSafe: DynamicObject
     {
-        private object o;
-        TypeInfo t;
-        object myLock;
+        private readonly object o;
+        private readonly TypeInfo t;
+        private readonly object myLock;
+
         /// <summary>
-        /// constructor
-        /// pass the object that you want to make
-        /// functions thread safe
+        /// Initializes a new instance of the <see cref="ThreadSafe"/> class.
         /// </summary>
-        /// <param name="o"></param>
+        /// <param name="o">
+        /// The wrapped object whose operations will be made thread-safe.
+        /// </param>
         public ThreadSafe(object o)
         {
             this.o = o;
             t = o.GetType().GetTypeInfo();
             myLock = new object();
         }
-        /// <summary>
-        /// redirects to original object
-        /// </summary>
-        /// <param name="binder"></param>
-        /// <param name="value"></param>
-        /// <returns></returns>
+
+        /// <inheritdoc />
         public override bool TrySetMember(SetMemberBinder binder, object value)
         {
-           
-            var field = t.GetDeclaredField(binder.Name);
-
-            lock (myLock)
+            var prop = t.GetDeclaredProperty(binder.Name);
+            if (prop != null)
             {
-                field.SetValue(o, value);
+                lock (myLock)
+                {
+                    prop.SetValue(o, value);
+                }
+
+                return true;
             }
-            return true;
+
+            var field = t.GetDeclaredField(binder.Name);
+            if (field != null)
+            {
+                lock (myLock)
+                {
+                    field.SetValue(o, value);
+                }
+
+                return true;
+            }
+
+            return false;
         }
-        /// <summary>
-        /// redirect to original object
-        /// </summary>
-        /// <param name="binder"></param>
-        /// <param name="result"></param>
-        /// <returns></returns>
+
+        /// <inheritdoc />
         public override bool TryGetMember(GetMemberBinder binder, out object result)
         {
-            
-            var field = t.GetDeclaredField(binder.Name);
-            
-            lock (myLock)
+            var prop = t.GetDeclaredProperty(binder.Name);
+            if (prop != null)
             {
-                result = field.GetValue(o);
+                lock (myLock)
+                {
+                    result = prop.GetValue(o);
+                }
+
+                return true;
             }
-            return true;
+
+            var field = t.GetDeclaredField(binder.Name);
+            if (field != null)
+            {
+                lock (myLock)
+                {
+                    result = field.GetValue(o);
+                }
+
+                return true;
+            }
+
+            result = null;
+            return false;
         }
+
         /// <summary>
-        /// redirects to original object
+        /// Attempts to find the indexer property.
         /// </summary>
-        /// <param name="binder"></param>
-        /// <param name="args"></param>
-        /// <param name="result"></param>
-        /// <returns></returns>
+        /// <returns>
+        /// The <see cref="PropertyInfo"/> for the indexer property, if found;
+        /// otherwise, <see langword="null"/>.
+        /// </returns>
+        private PropertyInfo GetIndexedProperty()
+        {
+            // TODO: Is there a better way to do this?
+
+            var prop = t.GetDeclaredProperty("Item");
+            if (prop == null || prop.GetIndexParameters().Length == 0)
+            {
+                prop = t.DeclaredProperties.FirstOrDefault(p => p.GetIndexParameters().Length != 0);
+            }
+
+            return prop;
+        }
+
+        /// <inheritdoc />
+        public override bool TrySetIndex(SetIndexBinder binder, object[] indexes, object value)
+        {
+            var prop = GetIndexedProperty();
+            if (prop != null)
+            {
+                lock (myLock)
+                {
+                    prop.SetValue(o, value, indexes);
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <inheritdoc />
+        public override bool TryGetIndex(GetIndexBinder binder, object[] indexes, out object result)
+        {
+            var prop = GetIndexedProperty();
+            if (prop != null)
+            {
+                lock (myLock)
+                {
+                    result = prop.GetValue(o, indexes);
+                }
+
+                return true;
+            }
+
+            result = null;
+            return false;
+        }
+
+        /// <inheritdoc />
         public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
         {
             var method = t.GetDeclaredMethod(binder.Name);
-
-            lock (myLock)
+            if (method != null)
             {
-                result = method.Invoke(this.o, args);
+                lock (myLock)
+                {
+                    result = method.Invoke(o, args);
+                }
+
+                return true;
             }
-            return true;
-            //return base.TryInvokeMember(binder, args, out result);
+
+            result = null;
+            return false;
         }
     }
 }
